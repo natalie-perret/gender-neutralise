82317b0c59f6f847859c4f594733f2fda1bf11cc
OEM <nperret@gmail.com>
2017-05-28 14:20:26 +1200

    CSS padding added

--- client/components/SearchBox.jsx
+++ client/components/SearchBox.jsx
@@ -24,8 +24,8 @@ export default class SearchBox extends React.Component {
     return (
       <div className="search-form">
         <form className="search-box-wrapper" action="/bias" method="get" onSubmit={this.handleSubmit.bind(this)}>
-          <input type="text" name='bias' placeholder='Enter your word' className="search-box-input" value={this.state.bias} onChange={this.handleChange.bind(this)} />
-          <input type='submit' value='Neutralise!' className="search-box-button"/>
+          <p><input type="text" name='bias' placeholder='Enter your word' className="search-box-input" value={this.state.bias} onChange={this.handleChange.bind(this)} /></p>
+          <p><input type='submit' value='Neutralise!' className="search-box-button"/></p>
         </form>
       </div>
     )
